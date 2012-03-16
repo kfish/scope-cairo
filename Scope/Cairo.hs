3f6273cc7e29497225d6451c055c6eadfa872c89
Conrad Parker <conrad@metadecks.org>
2012-03-16 09:43:35 +0800

    Events: move button, key events to library

--- Scope/Cairo.hs
+++ Scope/Cairo.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# OPTIONS -Wall -fno-warn-orphans -fno-warn-unused-do-bind #-}
 
@@ -40,9 +41,26 @@ data ViewCairo = ViewCairo
     }
 
 scopeCairoNew :: G.DrawingArea -> G.Adjustment -> IO (IORef (Scope ViewCairo))
-scopeCairoNew c adj = do
-    scopeRef <- newIORef $ scopeNew (viewCairoInit c adj)
+scopeCairoNew drawingArea adj = do
+    scopeRef <- newIORef $ scopeNew (viewCairoInit drawingArea adj)
+
     adj `G.onValueChanged` (scroll scopeRef)
+
+    drawingArea `G.on` G.buttonPressEvent $ G.tryEvent $ buttonDown scopeRef
+    drawingArea `G.on` G.buttonReleaseEvent $ G.tryEvent $ buttonRelease scopeRef
+    drawingArea `G.on` G.scrollEvent $ G.tryEvent $ wheel scopeRef
+    drawingArea `G.on` G.motionNotifyEvent $ G.tryEvent $ motion scopeRef
+    drawingArea `G.on` G.keyPressEvent $ G.tryEvent $ keyDown scopeRef
+    G.widgetAddEvents drawingArea
+        [ G.KeyPressMask
+        , G.KeyReleaseMask
+        , G.PointerMotionMask
+        , G.ButtonMotionMask
+        , G.ScrollMask
+        ]
+
+    G.widgetSetCanFocus drawingArea True
+
     return scopeRef
 
 viewCairoInit :: G.DrawingArea -> G.Adjustment -> ViewCairo
@@ -120,4 +138,79 @@ scroll :: IORef (Scope ViewCairo) -> IO ()
 scroll ref = do
     val <- G.adjustmentGetValue =<< adj . viewUI . view <$> readIORef ref
     scopeModifyUpdate ref (viewMoveTo val)
+
+----------------------------------------------------------------
+
+_canvasToScreen :: G.DrawingArea -> CanvasX -> IO ScreenX
+_canvasToScreen c (CanvasX cX) = do
+    (width, _height) <- G.widgetGetSize c
+    return $ ScreenX (fromIntegral width * cX)
+
+screenToCanvas :: ViewCairo -> ScreenX -> IO CanvasX
+screenToCanvas vc (ScreenX sX) = do
+    (width, _height) <- G.widgetGetSize (canvas vc)
+    return $ CanvasX (sX / fromIntegral width)
+
+----------------------------------------------------------------
+
+buttonDown :: IORef (Scope ViewCairo) -> G.EventM G.EButton ()
+buttonDown ref = do
+    (x, _y) <- G.eventCoordinates
+    liftIO $ do
+        vc <- viewUI . view <$> readIORef ref
+        cX <- screenToCanvas vc (ScreenX x)
+        scopeModifyUpdate ref (viewButtonDown cX)
+
+buttonRelease :: IORef (Scope ViewCairo) -> G.EventM G.EButton ()
+buttonRelease ref = liftIO $ modifyIORef ref (scopeModifyView viewButtonRelease)
+
+motion :: IORef (Scope ViewCairo) -> G.EventM G.EMotion ()
+motion ref = do
+    (x, _y) <- G.eventCoordinates
+    liftIO $ do
+        View{..} <- view <$> readIORef ref
+        cX <- screenToCanvas viewUI (ScreenX x)
+        scopeModifyUpdate ref (viewButtonMotion cX)
+
+wheel :: IORef (Scope ViewCairo) -> G.EventM G.EScroll ()
+wheel ref = do
+    (x, _y) <- G.eventCoordinates
+    dir <- G.eventScrollDirection
+    liftIO $ do
+        scope <- readIORef ref
+        let View{..} = view scope
+        cX <- screenToCanvas viewUI (ScreenX x)
+        case dir of
+            G.ScrollUp   -> scopeModifyUpdate ref (viewZoomInOn cX 1.2)
+            G.ScrollDown -> scopeModifyUpdate ref (viewZoomOutOn cX 1.2)
+            _            -> return ()
+
+----------------------------------------------------------------
+
+-- Some keys we are interested in, from:
+-- http://cgit.freedesktop.org/xorg/proto/x11proto/plain/keysymdef.h
+#define XK_Home                          0xff50
+#define XK_Left                          0xff51  /* Move left, left arrow */
+#define XK_Up                            0xff52  /* Move up, up arrow */
+#define XK_Right                         0xff53  /* Move right, right arrow */
+#define XK_Down                          0xff54  /* Move down, down arrow */
+#define XK_Page_Up                       0xff55
+#define XK_Page_Down                     0xff56
+#define XK_End                           0xff57  /* EOL */
+
+keyDown :: IORef (Scope ViewCairo) -> G.EventM G.EKey ()
+keyDown ref = do
+    v <- G.eventKeyVal
+    -- n <- G.eventKeyName
+    -- liftIO . putStrLn $ printf "Key %s (%d) pressed" n v
+    let f = case v of
+                XK_Home -> Just viewMoveStart
+                XK_End  -> Just viewMoveEnd
+                XK_Up   -> Just $ viewZoomIn 2.0
+                XK_Down -> Just $ viewZoomOut 2.0
+                XK_Left  -> Just viewMoveRight
+                XK_Right -> Just viewMoveLeft
+                _ -> Nothing
+
+    maybe (return ()) (liftIO . scopeModifyUpdate ref) f
 
